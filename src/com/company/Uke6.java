47629fcf8559115b257ca9bac6018b2cde23deef
AslakWold <s331378@oslomet.no>
2019-10-29 20:27:02 +0100

    +fiksa det problemet mitt med versjon

--- src/com/company/Uke6.java
+++ src/com/company/Uke6.java
@@ -8,7 +8,14 @@ public class Uke6 {
     public static void main(String[] args) {
         String[] s = {"Per", "null", null,  "Kari", "Ole", null, "Azra", null};
         Liste<String> liste = new TabellListe<>(s);
-        System.out.println("Hello");
+        System.out.println(liste.antall());
+        System.out.println(liste.hent(3));
+        System.out.println(liste.tom());
+        System.out.println(liste.indeksTil("Ole"));
+        System.out.println(liste.inneholder("null"));
+        System.out.println();
+
+        System.out.println(liste.toString());
 
     }
 }
--- src/hjelpeklasser/TabellListe.java
+++ src/hjelpeklasser/TabellListe.java
@@ -14,26 +14,26 @@ public class TabellListe<T> implements Liste<T> {
          antall = 0;                            //foreløpig ingen verdier
     }
 
-     public TabellListe() {              //Standardkonstruktør
-         this(10);              //Startstørrelse på 10
-     }
+    public TabellListe() {              //Standardkonstruktør
+        this(10);              //Startstørrelse på 10
+    }
 
-     public TabellListe(T[] b) {        //en T-tabell som parameter
-         this(b.length);                //Kaller den andre konstruktøren
-         for (T verdi : b) {
-             if (verdi != null) {
-                 a[antall++] = verdi;       //hopper over null-verdier
-             }
-         }
-     }
+    public TabellListe(T[] b) {        //en T-tabell som parameter
+        this(b.length);                //Kaller den andre konstruktøren
+        for (T verdi : b) {
+            if (verdi != null) {
+                a[antall++] = verdi;       //hopper over null-verdier
+            }
+        }
+    }
 
-     public int antall() {
-         return antall;         //returnerer antallet
-     }
+    public int antall() {
+        return antall;         //returnerer antallet
+    }
 
-     public boolean tom() {
-         return antall == 0;        //listen er tom hvis antall er 0
-     }
+    public boolean tom() {
+        return antall == 0;        //listen er tom hvis antall er 0
+    }
 
     @Override
     public void nullstill() {
@@ -46,16 +46,16 @@ public class TabellListe<T> implements Liste<T> {
     }
 
     public T hent(int indeks) {
-         indeksKontroll(indeks, false);     //false: indeks = antall er ulovlig
-         return a[indeks];                          //returnerer tabellelement
-     }
+        indeksKontroll(indeks, false);     //false: indeks = antall er ulovlig
+        return a[indeks];                          //returnerer tabellelement
+    }
 
-     public int indeksTil(T verdi) {
-         for(int i = 0; i < antall; i++) {
-             if(a[i].equals(verdi)) { return i; }  //funnet!
-         }
-         return -1;     //ikke funnet!
-     }
+    public int indeksTil(T verdi) {
+        for(int i = 0; i < antall; i++) {
+            if(a[i].equals(verdi)) { return i; }  //funnet!
+        }
+        return -1;     //ikke funnet!
+    }
 
     @Override
     public T oppdater(int indeks, T verdi) {
@@ -85,4 +85,14 @@ public class TabellListe<T> implements Liste<T> {
     public boolean inneholder(T verdi) {
          return indeksTil(verdi) != -1;
      }
+
+    public String toString() {
+       if(antall == 0) {return "[]";}
+       StringJoiner sj = new StringJoiner(", ","[", "]");
+
+       for(int i = 0; i < antall; i++) {
+           sj.add(a[i].toString());
+       }
+       return sj.toString();
+    }
 }
